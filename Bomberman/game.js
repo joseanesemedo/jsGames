57bb30a40cacb3decf9c71700bdd3eb822159ae9
Joseane Semedo <joseanelmsemedo2@gmail.com>
2023-03-07 19:48:57 +0000

    added map structure

--- Bomberman/game.js
+++ Bomberman/game.js
@@ -6,16 +6,50 @@ kaboom({
   clearColor: [0, 0, 0, 1],
 });
 
+loadSprite("wall-steel", "./sprites/wall.png");
+loadSprite("wall-brick", "./sprites/brick.png");
+loadSprite("door", "./sprites/door.png");
+loadSprite("explosion", "./sprites/explosion.png");
+loadSprite("brick-wood", "./sprites/wood.png");
+loadSprite("wall-gold", "./sprites/gold.png");
+loadSprite("bg", "./sprites/bg.png");
+
 scene("game", () => {
   layers(["bg", "obj", "ui"], "obj");
 
-  loadSprite("wall", "./sprites/wall.png");
-  loadSprite("brick", "./sprites/brick.png");
-  loadSprite("door", "./sprites/door.png");
-  loadSprite("explosion", "./sprites/explosion.png");
-  loadSprite("wood", "./sprites/wood.png");
-  loadSprite("gold", "./sprites/gold.png");
-  loadSprite("bg", "./sprites/bg.png");
+  const maps = [
+    [
+      "aaaaaaaaaaaaaaa",
+      "a             a",
+      "a             a",
+      "a             a",
+      "a             a",
+      "a             a",
+      "a             a",
+      "a             a",
+      "a             a",
+      "a             a",
+      "a             a",
+      "a             a",
+      "a             a",
+      "a             a",
+      "aaaaaaaaaaaaaaa",
+    ],
+  ];
+
+  const levelsCfg = {
+    width: 26,
+    height: 26,
+    a: () => [sprite("wall-steel"), "wall-steel", area(), solid(), "wall"],
+    z: () => [sprite("brick-red"), "wall-brick", area(), solid(), "wall"],
+    d: () => [sprite("brick-red"), "wall-brick-door", area(), solid(), "wall"],
+    b: () => [sprite("wall-gold"), "wall-gold", area(), solid(), "wall"],
+    w: () => [sprite("brick-wood"), "wall-brick", area(), solid(), "wall"],
+    p: () => [sprite("brick-wood"), "wall-brick-door", area(), solid(), "wall"],
+    t: () => [sprite("door"), "door", "wall"],
+  };
+
+  const gameLevel = addLevel(maps[0], levelsCfg);
 });
 
 go("game");
