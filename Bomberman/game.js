055eb50fa1e07bf55904cc864f4d8792cd6e52f9
Joseane Semedo <joseanelmsemedo2@gmail.com>
2023-03-07 20:20:54 +0000

    added player movement

--- Bomberman/game.js
+++ Bomberman/game.js
@@ -6,13 +6,33 @@ kaboom({
   clearColor: [0, 0, 0, 1],
 });
 
+const MOVE_SPEED = 120;
+
 loadSprite("wall-steel", "./sprites/wall.png");
-loadSprite("wall-brick", "./sprites/brick.png");
+loadSprite("brick-red", "./sprites/brick.png");
 loadSprite("door", "./sprites/door.png");
 loadSprite("explosion", "./sprites/explosion.png");
 loadSprite("brick-wood", "./sprites/wood.png");
 loadSprite("wall-gold", "./sprites/gold.png");
 loadSprite("bg", "./sprites/bg.png");
+
+loadSprite("bomberman", "./sprites/player.png", {
+  sliceX: 7,
+  sliceY: 4,
+  anims: {
+    // idle
+    idleLeft: { from: 21, to: 21 },
+    idleRight: { from: 7, to: 7 },
+    idleUp: { from: 0, to: 0 },
+    idleDown: { from: 14, to: 14 },
+
+    // move
+    moveLeft: { from: 22, to: 27, loop: true },
+    moveRight: { from: 8, to: 13, loop: true },
+    moveUp: { from: 1, to: 6, loop: true },
+    moveDown: { from: 15, to: 20, loop: true },
+  },
+});
 
 scene("game", () => {
   layers(["bg", "obj", "ui"], "obj");
@@ -50,6 +70,73 @@ scene("game", () => {
   };
 
   const gameLevel = addLevel(maps[0], levelsCfg);
+
+  const player = add([
+    sprite("bomberman", {
+      animeSpeed: 0.1,
+      frame: 14,
+    }),
+    pos(30, 190),
+    { dir: vec2(1, 0) },
+  ]);
+
+  //   player.action(() => {
+  //     player.pushOutAll();
+  //   });
+
+  //   movement
+  onKeyDown("left", () => {
+    player.move(-MOVE_SPEED, 0);
+    // h, v
+    player.dir = vec2(-1, 0);
+  });
+
+  onKeyDown("right", () => {
+    player.move(MOVE_SPEED, 0);
+    player.dir = vec2(1, 0);
+  });
+
+  onKeyDown("up", () => {
+    player.move(0, -MOVE_SPEED);
+    player.dir = vec2(0, -1);
+  });
+
+  onKeyDown("down", () => {
+    player.move(0, MOVE_SPEED);
+    player.dir = vec2(0, 1);
+  });
+
+  //   animations
+  onKeyPress("left", () => {
+    player.play("moveLeft");
+  });
+
+  onKeyPress("right", () => {
+    player.play("moveRight");
+  });
+
+  onKeyPress("up", () => {
+    player.play("moveUp");
+  });
+
+  onKeyPress("down", () => {
+    player.play("moveDown");
+  });
+
+  onKeyRelease(["left"], () => {
+    player.play("idleLeft");
+  });
+
+  onKeyRelease(["right"], () => {
+    player.play("idleRight");
+  });
+
+  onKeyRelease(["up"], () => {
+    player.play("idleUp");
+  });
+  onKeyRelease(["down"], () => {
+    player.play("idleDown");
+  });
 });
 
 go("game");
